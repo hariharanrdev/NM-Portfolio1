1788773d68b9005a0eb6cacdb6891a44a60df4c2
IThariharan <hariharanpgpcet@gmail.com>
2024-11-05 10:09:40 +0530

    files

---
+++ src/resumeData.js
@@ -0,0 +1,95 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Hariharan R",
+    "role": "Backend Developer & Editor",
+    "linkedinId":"Hariharanr04",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I enjoy exploring various aspects of backend development using Python, learning about new technologies, and writing technical articles. In my free time, I like to relax by playing games.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/hariharanr04/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/IThariharan",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"code chef",
+          "url":"https://www.codechef.com/users/hariharanr03",
+          "className":"fa fa-code chef"
+        }
+      ],
+    "aboutme":"I'm currently a final-year B.E. CSE student at PGP College of Engineering and Technology. As a self-taught backend developer, I'm now focused on deepening my skills in web development. I believe that achieving success requires dedication to one’s dreams and persistent effort to realize them.",
+    "address":"India",
+    "education":[
+      {
+        "UniversityName":"PGP College of Engineering and Technology",
+        "specialization":"Computer Science and Engineering",
+        "MonthOfPassing":"JUN",
+        "YearOfPassing":"2025",
+        "Achievements":"7.7 CGPA"
+      },
+      {
+        "UniversityName":"P.A.Vidhya Bhavan Matric HrSec School,Karur",
+        "specialization":"HSC",
+        "MonthOfPassing":"MAR",
+        "YearOfPassing":"2021",
+        "Achievements":"71.4"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"IBM-Cloud Static Apps",
+        "specialization":"Personal Blog",
+        "MonthOfLeaving":"NOV",
+        "YearOfLeaving":"2023",
+        "Achievements":"Backend Developer"
+      },
+      {
+        "CompanyName":"TO-DO List Application",
+        "specialization":"Python",
+        "MonthOfLeaving":"APR",
+        "YearOfLeaving":"2024",
+        "Achievements":"Connectted with SQL"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"Java"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+  }
+  
+  export default resumeData
